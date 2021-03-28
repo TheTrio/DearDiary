9c4eefc01e3840aa1a918ddd5b8bc9456594f10a
Shashwat Khanna <shashwatkhanna312@gmail.com>
2021-03-29 01:37:43 +0530

    added theme switcher

---
+++ public/js/settings.js
@@ -0,0 +1,23 @@
+const lightRadio = document.getElementById('lightRadio')
+const darkRadio = document.getElementById('darkRadio')
+let selectedRadio = darkRadio
+
+lightRadio.addEventListener('click', (e) => {
+	if (selectedRadio !== lightRadio) {
+		const body = document.getElementsByTagName('body')[0]
+		body.classList.remove('dark')
+		body.classList.add('light')
+		selectedRadio = lightRadio
+		console.log('Running')
+	}
+
+})
+
+darkRadio.addEventListener('click', (e) => {
+	if (selectedRadio !== darkRadio) {
+		const body = document.getElementsByTagName('body')[0]
+		body.classList.remove('light')
+		body.classList.add('dark')
+		selectedRadio = darkRadio
+	}
+})
--- public/js/stopTransition.js
+++ public/js/stopTransition.js
@@ -1,10 +1,24 @@
 stopResponsiveTransition();
-
+const hideUserIcon = (e) => {
+	console.log('HIDING')
+	userIcon.classList.toggle('hide')
+}
+let triggerHideUserIcon = window.innerWidth <= 700
+if (triggerHideUserIcon) {
+	hamburger.addEventListener('click', hideUserIcon)
+}
 function stopResponsiveTransition() {
 	const leftClass = document.getElementById('left').classList
 	const rightClass = document.getElementById('right_pane').classList
 	let timer = null;
 	window.addEventListener('resize', function () {
+		if (!triggerHideUserIcon && window.innerWidth <= 700) {
+			hamburger.addEventListener('click', hideUserIcon)
+			triggerHideUserIcon = true
+		} else if (triggerHideUserIcon) {
+			hamburger.removeEventListener('click', hideUserIcon)
+			triggerHideUserIcon = false
+		}
 		console.log('resizing')
 		if (timer) {
 			clearTimeout(timer);
