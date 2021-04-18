48d4b5a6f699649079bbf5ebf2ef413e7cc69cd3
Shashwat <10794178+TheTrio@users.noreply.github.com>
2021-04-18 12:32:16 +0530

    increased body json limit to 50mb to allow images

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const sessionConfig = {
 }
 
 // making sure req.body is parsed correctly
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
 
 // setting up EJS
