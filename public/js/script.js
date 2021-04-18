3a9875bd7fb4dd9c3a19a029cf73d64fc9e7cd68
Shashwat <10794178+TheTrio@users.noreply.github.com>
2021-04-18 11:44:21 +0530

    added imgur integration

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,27 @@
 Quill.register('modules/counter', function (quill, options) {
-    var container = document.querySelector(options.container);
+    var container = document.querySelector(options.container)
     quill.on('text-change', function () {
-        var text = quill.getText();
+        var text = quill.getText()
         if (options.unit === 'word') {
-            container.innerText = text.split(/\s+/).length - 1 + ' words';
+            container.innerText = text.split(/\s+/).length - 1 + ' words'
         } else {
-            container.innerText = text.length + ' characters';
+            container.innerText = text.length + ' characters'
         }
-    });
-});
+    })
+})
 
 var quill = new Quill('#editor', {
     modules: {
         counter: {
             container: '#words',
-            unit: 'word'
+            unit: 'word',
         },
-        toolbar: true
+        toolbar: true,
     },
     theme: 'snow',
     scrollingContainer: '#textbox',
-    placeholder: 'Your Entry goes here'
-});
+    placeholder: 'Your Entry goes here',
+})
 const flashDismissButton = document.getElementsByClassName('dismiss')[0]
 const saveButton = document.getElementsByClassName('saveNow')[0]
 const title = document.getElementById('title')
@@ -49,10 +49,10 @@ flashDismissButton.addEventListener('click', (e) => {
 })
 
 error_btn.addEventListener('click', (e) => {
-    window.location = "/entry/new";
+    window.location = '/entry/new'
 })
 
-userIcon.addEventListener('click', e => {
+userIcon.addEventListener('click', (e) => {
     right_pane.classList.toggle('moveLeft')
     leftPane.classList.remove('moveRight')
     bar.classList.remove('moveRight')
@@ -76,14 +76,14 @@ const dustbinClick = (entry, e) => {
             const options = {
                 method: 'DELETE',
                 headers: {
-                    'Content-Type': 'application/json'
-                }
+                    'Content-Type': 'application/json',
+                },
             }
             fetch(`/entry/${entry.id}`, options)
                 .then((resp) => resp.text())
                 .then((data) => {
                     entry.parentElement.remove()
-                    window.location = "/entry/new";
+                    window.location = '/entry/new'
                 })
         }
     }
@@ -97,24 +97,30 @@ const updateCurrentEntry = (id, title, date) => {
     console.log('runnig again')
     current_entry.innerHTML = ''
 
-    current_entry.insertAdjacentHTML('afterbegin', `<li>
+    current_entry.insertAdjacentHTML(
+        'afterbegin',
+        `<li>
                 <div class="entry_item selected" id="${id}">
                     <div>
                         <a href="/entry/${id}">${title} </a>
                     </div>
                     <div id="date_label">
-                        ${new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(date)}
+                        ${new Intl.DateTimeFormat('en-US', {
+                            weekday: 'long',
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                        }).format(date)}
                     </div>
                 </div>
-        </li> `)
+        </li> `
+    )
     const c = current_entry.querySelector(`div[id="${id}"]`)
-    c.addEventListener('hover', e => {
+    c.addEventListener('hover', (e) => {
         dustbinClick(c, e)
     })
-
 }
-date.value = (new Date()).toISOString().slice(0, 10)
-
+date.value = new Date().toISOString().slice(0, 10)
 
 title.addEventListener('keyup', () => {
     if (title.value.length > 0) {
@@ -149,35 +155,74 @@ saveButton.addEventListener('click', () => {
         const Entry = {
             Delta: Delta,
             date: chosenDate,
-            title: title.value
+            title: title.value,
         }
         console.log('running')
         const options = {
             method: 'PATCH',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(Entry)
+            body: JSON.stringify(Entry),
         }
-        fetch(`/entry/${id}`, options).then(resp => resp.text()).then(d => {
-            console.log(d)
-            entry_flash.querySelector('.flash_heading').innerHTML = 'Entry Saved'
-            entry_flash.classList.remove('dismissed')
-            entry_flash.classList.add('visible')
-            setTimeout(() => {
-                entry_flash.classList.remove('visible')
-                entry_flash.classList.add('dismissed')
-            }, 5000)
-            if (d === 'DONE!') {
-                updateCurrentEntry(id, Entry.title, Entry.date)
-            }
-        })
+        fetch(`/entry/${id}`, options)
+            .then((resp) => resp.text())
+            .then((d) => {
+                console.log(d)
+                entry_flash.querySelector('.flash_heading').innerHTML =
+                    'Entry Saved'
+                entry_flash.classList.remove('dismissed')
+                entry_flash.classList.add('visible')
+                setTimeout(() => {
+                    entry_flash.classList.remove('visible')
+                    entry_flash.classList.add('dismissed')
+                }, 5000)
+                if (d === 'DONE!') {
+                    updateCurrentEntry(id, Entry.title, Entry.date)
+                }
+            })
     }
 })
-quill.on('text-change', function (delta, oldDelta, source) {
-    console.log(delta)
+
+const uploadBase64Img = async (image) => {
+    const data = { image }
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+    var url = ''
+    return fetch('/api/upload', requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            url = result.url
+            console.log(url)
+            return url
+        })
+        .catch((error) => console.log('error', error))
+}
+quill.on('text-change', async function (delta, oldDelta, source) {
+    // checking if image was added
+
+    const imgs = Array.from(
+        editor.querySelectorAll('img[src^="data:"]:not(.loading)')
+    )
+    for (const img of imgs) {
+        console.log('Hello')
+        img.classList.add('loading')
+        uploadBase64Img(
+            img.getAttribute('src').replace('data:image/png;base64,', '')
+        ).then((resp) => {
+            console.log('Editing')
+            img.setAttribute('src', resp)
+            img.classList.remove('loading')
+        })
+    }
+
     if (editor.classList.contains('invalid')) {
         editor.classList.remove('invalid')
         editor.classList.add('valid')
     }
-});
+})
--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const { isLoggedIn, isEntryAuthor } = require('../utils/Middleware')
 const { wrapAsync } = require('../utils/wrapAsync')
+const ImgurAnonymousUploader = require('imgur-anonymous-uploader')
 
 const router = express.Router()
 
-router.get('/entry/:id', isLoggedIn, isEntryAuthor, wrapAsync(async (req, res) => {
-	const { id } = req.params
-	const entry = req.entry
-	if (entry === null) {
-		throw new AppError('No such Entry found', 404)
-	}
-	res.send(entry)
-}))
+router.get(
+    '/entry/:id',
+    isLoggedIn,
+    isEntryAuthor,
+    wrapAsync(async (req, res) => {
+        const { id } = req.params
+        const entry = req.entry
+        if (entry === null) {
+            throw new AppError('No such Entry found', 404)
+        }
+        res.send(entry)
+    })
+)
+
+router.post(
+    '/upload',
+    wrapAsync(async (req, res) => {
+        try {
+            // converts base64 string to Buffer
+            // For some reason, couldn't use the imgur API manually. Had to use this package which made things a lot simpler
+            const imageBase64 = Buffer.from(req.body.image, 'base64')
+            const uploader = new ImgurAnonymousUploader(
+                process.env.imgurClientID
+            )
+            const response = await uploader.uploadBuffer(imageBase64)
+            console.log(response)
+            res.send(response)
+        } catch (e) {
+            console.log(e)
+            res.send(e)
+        }
+    })
+)
 
 module.exports = router
