a17e8b0025febb73dc0a9e915f8dc169fbfb7883
Shashwat <shashwatkhanna312@gmail.com>
2021-05-13 22:30:02 +0530

    added navigation buttons

--- public/js/setEntry.js
+++ public/js/setEntry.js
@@ -16,22 +16,34 @@ const setEntry = (id) => {
             date.value = data.date.slice(0, 10)
             document.title = `Diary Entry - ${data.title}`
             const selected = document.createElement('li')
-            selected.insertAdjacentHTML('afterbegin', `
+            selected.insertAdjacentHTML(
+                'afterbegin',
+                `
                 <div class="entry_item" id="${id}">
                     <div>
                         <a href="/entry/${id} ">${data.title}</a>
                     </div>
                     <div id="date_label">
-                        ${new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(data.date))} 
+                        ${new Intl.DateTimeFormat('en-US', {
+                            weekday: 'long',
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                        }).format(new Date(data.date))} 
                     </div>
                 </div>
-                `)
+                `
+            )
             const c = selected.querySelector(`div[id="${id}"]`)
             c.addEventListener('click', (e) => {
                 dustbinClick(c, e)
             })
             c.classList.add('selected')
             current_entry.insertAdjacentElement('afterbegin', selected)
+            if (data.prev !== null)
+                leftNavBtn.querySelector('a').setAttribute('href', `/entry/${data.prev}`)
+            if (data.next !== null)
+                rightNavBtn.querySelector('a').setAttribute('href', `/entry/${data.next}`)
             setTimeout(() => {
                 loading_screen.classList.remove('loading')
                 document.getElementsByTagName('body')[0].classList.remove('unscroll')
--- routes/api.js
+++ routes/api.js
@@ -6,16 +6,21 @@ const { decryptEntry } = require('../utils/crypto')
 
 const router = express.Router()
 
-router.get('/entry/:id', isLoggedIn, isEntryAuthor, wrapAsync(async (req, res) => {
-    const { id } = req.params
-    const { _id, Delta, date, title, owner } = req.entry
-    const decryptedEntry = decryptEntry(Delta, req.session.key)
-    const entry = { _id, Delta: decryptedEntry, date, title, owner }
-    if (entry === null) {
-        throw new AppError('No such Entry found', 404)
-    }
-    res.send(entry)
-}))
+router.get(
+    '/entry/:id',
+    isLoggedIn,
+    isEntryAuthor,
+    wrapAsync(async (req, res) => {
+        const { id } = req.params
+        const { _id, Delta, date, title, owner, prev = null, next = null } = req.entry
+        const decryptedEntry = decryptEntry(Delta, req.session.key)
+        const entry = { _id, Delta: decryptedEntry, date, title, owner, prev, next }
+        if (entry === null) {
+            throw new AppError('No such Entry found', 404)
+        }
+        res.send(entry)
+    })
+)
 
 router.post(
     '/upload',
@@ -24,9 +29,7 @@ router.post(
             // converts base64 string to Buffer
             // For some reason, couldn't use the imgur API manually. Had to use this package which made things a lot simpler
             const imageBase64 = Buffer.from(req.body.image, 'base64')
-            const uploader = new ImgurAnonymousUploader(
-                process.env.imgurClientID
-            )
+            const uploader = new ImgurAnonymousUploader(process.env.imgurClientID)
             const response = await uploader.uploadBuffer(imageBase64)
             console.log(response)
             res.send(response)
