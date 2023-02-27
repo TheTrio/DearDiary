398f2a4e1e7a5696ac46cdca18928cab004d1c70
Shashwat <10794178+TheTrio@users.noreply.github.com>
2023-02-28 01:40:54 +0530

    remove unused api endpoints

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,36 @@
 function isEditablePage() {
-    if (location.href.includes('edit') || location.href.includes('new')) {
-        return [
-            'bold',
-            'italic',
-            'heading',
-            '|',
-            'quote',
-            'unordered-list',
-            'ordered-list',
-            '|',
-            'link',
-            'image',
-            'table',
-            'horizontal-rule',
-            '|',
-            'side-by-side',
-            'fullscreen',
-            '|',
-            'guide',
-        ]
-    }
-    return false
+  if (location.href.includes('edit') || location.href.includes('new')) {
+    return [
+      'bold',
+      'italic',
+      'heading',
+      '|',
+      'quote',
+      'unordered-list',
+      'ordered-list',
+      '|',
+      'link',
+      'image',
+      'table',
+      'horizontal-rule',
+      '|',
+      'side-by-side',
+      'fullscreen',
+      '|',
+      'guide',
+    ]
+  }
+  return false
 }
 
 const simplemde = new SimpleMDE({
-    element: document.getElementById('markdownEditor'),
-    spellChecker: false,
-    toolbar: isEditablePage(),
+  element: document.getElementById('markdownEditor'),
+  spellChecker: false,
+  toolbar: isEditablePage(),
 })
 
 if (isEditablePage()) {
-    document.querySelector('html').classList.add('edit')
+  document.querySelector('html').classList.add('edit')
 }
 simplemde.codemirror.options.readOnly = true
 simplemde.codemirror.options.nocursor = true
@@ -59,155 +59,134 @@ const leftNavBtn = document.getElementById('leftNavBtn')
 const rightNavBtn = document.getElementById('rightNavBtn')
 
 flashDismissButton.addEventListener('click', (e) => {
-    entry_flash.classList.add('dismissed')
+  entry_flash.classList.add('dismissed')
 })
 
 error_btn.addEventListener('click', (e) => {
-    window.location = '/entry/new'
+  window.location = '/entry/new'
 })
 
 userIcon.addEventListener('click', (e) => {
-    right_pane.classList.toggle('moveLeft')
-    leftPane.classList.remove('moveRight')
-    bar.classList.remove('moveRight')
+  right_pane.classList.toggle('moveLeft')
+  leftPane.classList.remove('moveRight')
+  bar.classList.remove('moveRight')
 })
 
 hamburger.addEventListener('click', (e) => {
-    leftPane.classList.toggle('moveRight')
-    bar.classList.toggle('moveRight')
-    content.classList.toggle('scrollable')
-    right_pane.classList.remove('moveLeft')
-    for (let item of entry_items) {
-        item.classList.toggle('showDustbin')
-    }
-    document.getElementsByTagName('body')[0].classList.toggle('unscroll')
+  leftPane.classList.toggle('moveRight')
+  bar.classList.toggle('moveRight')
+  content.classList.toggle('scrollable')
+  right_pane.classList.remove('moveLeft')
+  for (let item of entry_items) {
+    item.classList.toggle('showDustbin')
+  }
+  document.getElementsByTagName('body')[0].classList.toggle('unscroll')
 })
 
 const dustbinClick = (entry, e) => {
-    if (e.offsetX > entry.offsetWidth) {
-        if (confirm('Are you sure you want to delete this entry?')) {
-            const options = {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-            fetch(`/entry/${entry.id}`, options)
-                .then((resp) => resp.text())
-                .then((data) => {
-                    entry.parentElement.remove()
-                    window.location = '/entry/new'
-                })
-        }
+  if (e.offsetX > entry.offsetWidth) {
+    if (confirm('Are you sure you want to delete this entry?')) {
+      const options = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      fetch(`/entry/${entry.id}`, options)
+        .then((resp) => resp.text())
+        .then((data) => {
+          entry.parentElement.remove()
+          window.location = '/entry/new'
+        })
     }
+  }
 }
 for (let entry of entry_items) {
-    entry.addEventListener('click', (e) => {
-        dustbinClick(entry, e)
-    })
+  entry.addEventListener('click', (e) => {
+    dustbinClick(entry, e)
+  })
 }
 const updateCurrentEntry = (id, title, date) => {
-    current_entry.innerHTML = ''
+  current_entry.innerHTML = ''
 
-    current_entry.insertAdjacentHTML(
-        'afterbegin',
-        `<li>
+  current_entry.insertAdjacentHTML(
+    'afterbegin',
+    `<li>
                 <div class="entry_item selected" id="${id}">
                     <div>
                         <a href="/entry/${id}">${title} </a>
                     </div>
                     <div id="date_label">
                         ${new Intl.DateTimeFormat('en-US', {
-                            weekday: 'long',
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
+                          weekday: 'long',
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
                         }).format(date)}
                     </div>
                 </div>
         </li> `
-    )
-    const c = current_entry.querySelector(`div[id="${id}"]`)
-    c.addEventListener('hover', (e) => {
-        dustbinClick(c, e)
-    })
+  )
+  const c = current_entry.querySelector(`div[id="${id}"]`)
+  c.addEventListener('hover', (e) => {
+    dustbinClick(c, e)
+  })
 }
 date.value = new Date().toISOString().slice(0, 10)
 
 title.addEventListener('keyup', () => {
-    if (title.value.length > 0) {
-        if (title.classList.contains('invalid')) {
-            title.classList.remove('invalid')
-            title.classList.add('valid')
-        }
+  if (title.value.length > 0) {
+    if (title.classList.contains('invalid')) {
+      title.classList.remove('invalid')
+      title.classList.add('valid')
     }
+  }
 })
 
 saveButton.addEventListener('click', () => {
-    const chosenDate = new Date(date.value)
-    if (simplemde.value().trim().length === 0) {
-        editor.classList.remove('valid')
-        editor.classList.add('invalid')
-        editor.classList.add('big')
-        setTimeout(() => {
-            editor.classList.remove('big')
-        }, 500)
-    } else if (title.value.length === 0) {
-        title.classList.remove('valid')
-        title.classList.add('invalid')
-        title.classList.add('big')
-        setTimeout(() => {
-            title.classList.remove('big')
-        }, 500)
-    } else if (id !== -1) {
-        editor.classList.remove('invalid')
-        const Entry = {
-            date: chosenDate,
-            title: title.value,
-            markdown: simplemde.value(),
-        }
-        const options = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(Entry),
-        }
-        fetch(`/entry/${id}`, options)
-            .then((resp) => resp.text())
-            .then((d) => {
-                console.log(d)
-                entry_flash.querySelector('.flash_heading').innerHTML =
-                    'Entry Saved'
-                entry_flash.classList.remove('dismissed')
-                entry_flash.classList.add('visible')
-                setTimeout(() => {
-                    entry_flash.classList.remove('visible')
-                    entry_flash.classList.add('dismissed')
-                }, 5000)
-                if (d === 'DONE!') {
-                    updateCurrentEntry(id, Entry.title, Entry.date)
-                }
-            })
+  const chosenDate = new Date(date.value)
+  if (simplemde.value().trim().length === 0) {
+    editor.classList.remove('valid')
+    editor.classList.add('invalid')
+    editor.classList.add('big')
+    setTimeout(() => {
+      editor.classList.remove('big')
+    }, 500)
+  } else if (title.value.length === 0) {
+    title.classList.remove('valid')
+    title.classList.add('invalid')
+    title.classList.add('big')
+    setTimeout(() => {
+      title.classList.remove('big')
+    }, 500)
+  } else if (id !== -1) {
+    editor.classList.remove('invalid')
+    const Entry = {
+      date: chosenDate,
+      title: title.value,
+      markdown: simplemde.value(),
     }
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(Entry),
+    }
+    fetch(`/entry/${id}`, options)
+      .then((resp) => resp.text())
+      .then((d) => {
+        console.log(d)
+        entry_flash.querySelector('.flash_heading').innerHTML = 'Entry Saved'
+        entry_flash.classList.remove('dismissed')
+        entry_flash.classList.add('visible')
+        setTimeout(() => {
+          entry_flash.classList.remove('visible')
+          entry_flash.classList.add('dismissed')
+        }, 5000)
+        if (d === 'DONE!') {
+          updateCurrentEntry(id, Entry.title, Entry.date)
+        }
+      })
+  }
 })
-
-const uploadBase64Img = async (image) => {
-    const data = { image }
-    var requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }
-    var url = ''
-    return fetch('/api/upload', requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-            url = result.url
-            console.log(url)
-            return url
-        })
-        .catch((error) => console.log('error', error))
-}
--- routes/api.js
+++ routes/api.js
@@ -7,58 +7,35 @@ const { decryptEntry } = require('../utils/crypto')
 const router = express.Router()
 
 router.get(
-    '/entry/:id',
-    isLoggedIn,
-    isEntryAuthor,
-    wrapAsync(async (req, res) => {
-        const { id } = req.params
-        const {
-            _id,
-            markdown,
-            date,
-            title,
-            owner,
-            prev = null,
-            next = null,
-        } = req.entry
-        const decryptedMarkdown = decryptEntry(
-            req.entry.markdown,
-            req.session.key
-        )
-        const entry = {
-            _id,
-            date,
-            title,
-            owner,
-            prev,
-            next,
-            markdown: decryptedMarkdown,
-        }
-        if (entry === null) {
-            throw new AppError('No such Entry found', 404)
-        }
-        res.send(entry)
-    })
-)
-
-router.post(
-    '/upload',
-    wrapAsync(async (req, res) => {
-        try {
-            // converts base64 string to Buffer
-            // For some reason, couldn't use the imgur API manually. Had to use this package which made things a lot simpler
-            const imageBase64 = Buffer.from(req.body.image, 'base64')
-            const uploader = new ImgurAnonymousUploader(
-                process.env.imgurClientID
-            )
-            const response = await uploader.uploadBuffer(imageBase64)
-            console.log(response)
-            res.send(response)
-        } catch (e) {
-            console.log(e)
-            res.send(e)
-        }
-    })
+  '/entry/:id',
+  isLoggedIn,
+  isEntryAuthor,
+  wrapAsync(async (req, res) => {
+    const { id } = req.params
+    const {
+      _id,
+      markdown,
+      date,
+      title,
+      owner,
+      prev = null,
+      next = null,
+    } = req.entry
+    const decryptedMarkdown = decryptEntry(req.entry.markdown, req.session.key)
+    const entry = {
+      _id,
+      date,
+      title,
+      owner,
+      prev,
+      next,
+      markdown: decryptedMarkdown,
+    }
+    if (entry === null) {
+      throw new AppError('No such Entry found', 404)
+    }
+    res.send(entry)
+  })
 )
 
 module.exports = router
