1cff52dbe509581e73352daf83a1f5e7a1c33308
Shashwat <shashwatkhanna312@gmail.com>
2024-06-11 23:09:27 +0530

    refine edit mode

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,6 +1,11 @@
 window.onbeforeunload = () => true
 if (id != -1) {
-    setEntry(id)
+  setEntry(id)
 }
 
 simplemde.codemirror.options.readOnly = false
+
+document.querySelector('.exitNow.btn').addEventListener('click', (e) => {
+  window.onbeforeunload = null
+  window.location = `/entry/${id}`
+})
